6a9da49d7e83f63f727039db9f3c7668455ebb4b
Shrikant114 <skntkush114@gmail.com>
2025-05-12 00:04:02 +0530

    alert remove

--- app.py
+++ app.py
@@ -70,4 +70,4 @@ def reveal():
     return redirect(url_for('index'))
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=10000) #for render
+    app.run(host='0.0.0.0', port=3000) #for render
